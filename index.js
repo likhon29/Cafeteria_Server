116dc4347b5d22d743fc23b65b28de3e902a733d
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2023-02-02 11:17:33 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.epizi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("JU-cafe").collection("userCollection");
+
+    app.post("/addUser", async (req, res) => {
+      const user = {
+        name: "Likhon1",
+        dept: "IIT2",
+      };
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+run().catch(console.log);
+
+app.get("/", (req, res) => {
+  res.send("Ju Cafeteria server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Ju cafe running on port ${port}`);
+});
