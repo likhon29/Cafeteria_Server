f6381d6cfc63fb1b10e67f03093434a694bd562e
Md Abdur Rouf Likhon <mdabdurrouf.likhon@gmail.com>
2023-03-22 23:06:02 +0600

    added

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 require("dotenv").config();
+const SSLCommerzPayment = require("sslcommerz-lts");
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASSWD;
+const is_live = false;
 
 const port = process.env.PORT || 5000;
 
@@ -26,6 +30,10 @@ async function run() {
       .db("JU-cafe")
       .collection("ordersCollection");
     const adminCollection = client.db("JU-cafe").collection("adminCollection");
+    const bookingCollection = client
+      .db("JU-cafe")
+      .collection("bookingCollection");
+
     // verify admin user
     const verifyAdmin = async (req, res, next) => {
       const decodedEmail = req.decoded.email;
@@ -137,14 +145,94 @@ async function run() {
     });
     // get a particular order of user
 
+    // reservations
+
+    app.post("/reservations", async (req, res) => {
+      const reservationInfo = req.body;
+      const result = await bookingCollection.insertOne(reservationInfo);
+      res.send(result);
+    });
+
+    //  all reservation of a particular user
+    app.get("/reservations", async (req, res) => {
+      const email = req.query.email;
+
+      const query = {
+        customerEmail: email,
+      };
+
+      // console.log(query);
+      const orders = await bookingCollection.find(query).toArray();
+      res.send(orders);
+    });
+
+
+
+
+    app.delete("/reservation/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reservationCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // food order
 
     app.post("/orders", async (req, res) => {
       const orders = req.body;
-
       const result = await ordersCollection.insertOne(orders);
       res.send(result);
     });
+
+
+//sslcommerz init
+app.post('/init',async (req, res) => {
+  const order=req.body;
+
+  const bookingInfo = await bookingCollection.findOne({_id:new ObjectId(order.id)})
+
+  console.log(bookingInfo)
+  const data = {
+      total_amount: 100,
+      currency: 'BDT',
+      tran_id: 'REF123', // use unique tran_id for each api call
+      success_url: 'http://localhost:3030/success',
+      fail_url: 'http://localhost:3030/fail',
+      cancel_url: 'http://localhost:3030/cancel',
+      ipn_url: 'http://localhost:3030/ipn',
+      shipping_method: 'Courier',
+      product_name: 'Computer.',
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name: 'Customer Name',
+      cus_email: 'customer@example.com',
+      cus_add1: 'Dhaka',
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone: '01711111111',
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
+  };
+  const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+  sslcz.init(data).then(apiResponse => {
+      // Redirect the user to payment gateway
+      let GatewayPageURL = apiResponse.GatewayPageURL
+      res.redirect(GatewayPageURL)
+      console.log('Redirecting to: ', GatewayPageURL)
+  });
+})
+
+    
+
   } finally {
   }
 }
